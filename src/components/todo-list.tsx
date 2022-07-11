1734cc5417c469784d18f263ff6d89ce30cab52a
Your Name <you@example.com>
2022-07-12 04:06:25 +0700

    add margins between todo items

--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -1,5 +1,6 @@
 import { accessStore } from "../store/store"
 import { TodoComponent } from "./todo-item"
+import "./todo-list.scss"
 
 export function TodoListComponent() {
   const { state } = accessStore()
@@ -7,7 +8,11 @@ export function TodoListComponent() {
   return (
     <>
       {state.todo.todoList.map(todo => (
-        <TodoComponent todo={todo}></TodoComponent>
+        <ul className="todo-list__list">
+          <li className="todo-list__list-item">
+            <TodoComponent todo={todo} key={todo.id}></TodoComponent>
+          </li>
+        </ul>
       ))}
     </>
   )
