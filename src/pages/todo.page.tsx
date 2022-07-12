d9f375e100cbc2645b5588225cb3c0191645a4ab
vulnerabbity <v@example.com>
2022-07-12 21:50:16 +0700

    add titles to todo page

--- src/pages/todo.page.tsx
+++ src/pages/todo.page.tsx
@@ -1,12 +1,38 @@
 import { AddTodoComponent } from "../components/add-todo/add-todo"
 import { TodoListComponent } from "../components/todo-list/todo-list"
+import { ActionDeleteAllTodo } from "../store/actions/todo"
+import { accessStore } from "../store/store"
+import "./todo.page.scss"
 
 export function TodoPage() {
+  const { dispatch, state } = accessStore()
+
+  function deleteAll() {
+    dispatch(ActionDeleteAllTodo())
+  }
+
+  const haveTodo = state.todo.todoList.length > 0
+
   return (
-    <div>
-      <div className="container">
-        <AddTodoComponent></AddTodoComponent>
-        <TodoListComponent></TodoListComponent>
+    <div className="todo-page">
+      <div className="todo-page__container container">
+        <section className="todo-page__section">
+          <div className="todo-page__section-header">
+            <h2 className="todo-page__section-title">Add todo</h2>
+          </div>
+          <AddTodoComponent></AddTodoComponent>
+        </section>
+        {haveTodo && (
+          <section className="todo-page__section">
+            <div className="todo-page__section-header">
+              <h2 className="todo-page__section-title">Todo list: </h2>
+              <button className="button" onClick={deleteAll}>
+                Delete all
+              </button>
+            </div>
+            <TodoListComponent></TodoListComponent>
+          </section>
+        )}
       </div>
     </div>
   )
