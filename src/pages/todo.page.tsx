e1f73a67c2f6e61a4576a20e79870469bf162b09
vulnerabbity <v@example.com>
2022-07-12 22:09:11 +0700

    add delete completed button

--- src/pages/todo.page.tsx
+++ src/pages/todo.page.tsx
@@ -1,6 +1,6 @@
 import { AddTodoComponent } from "../components/add-todo/add-todo"
 import { TodoListComponent } from "../components/todo-list/todo-list"
-import { ActionDeleteAllTodo } from "../store/actions/todo"
+import { ActionDeleteAllCompletedTodo, ActionDeleteAllTodo } from "../store/actions/todo"
 import { accessStore } from "../store/store"
 import "./todo.page.scss"
 
@@ -9,6 +9,10 @@ export function TodoPage() {
 
   function deleteAll() {
     dispatch(ActionDeleteAllTodo())
+  }
+
+  function deleteAllFinished() {
+    dispatch(ActionDeleteAllCompletedTodo())
   }
 
   const haveTodo = state.todo.todoList.length > 0
@@ -26,9 +30,14 @@ export function TodoPage() {
           <section className="todo-page__section">
             <div className="todo-page__section-header">
               <h2 className="todo-page__section-title">Todo list: </h2>
-              <button className="button" onClick={deleteAll}>
-                Delete all
-              </button>
+              <div className="todo-page__buttons-group">
+                <button className="button" onClick={deleteAll}>
+                  Delete all
+                </button>
+                <button className="button" onClick={deleteAllFinished}>
+                  Delete finished
+                </button>
+              </div>
             </div>
             <TodoListComponent></TodoListComponent>
           </section>
